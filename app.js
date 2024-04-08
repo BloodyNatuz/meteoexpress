34555f5fce7a09152ad8c4f2927e60734e83e27e
BloodyNatuz <nathuznoepupin@gmail.com>
2024-04-08 15:05:23 +0200

    🧐 getall ville

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express');
-const app = express();
-const port = 4000;
+var Twig = require("twig"),
+    express = require('express'),
+    app = express();
+var db = require("./database.js")
+
+// Twig options
+app.set("twig options", {
+    allowAsync: true,
+    strict_variables: false
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('./index.twig', {
+        message: "Hello world!",
+        pageName: "Accueil"
+    })
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get('/api/villes', (req, res, next) => {
+    var sql = "select * from ville";
+    var params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        } else{
+            res.json({
+                rows
+            });
+        }
+    });
+})
+
+app.listen(4000, () => {
+    console.log(`Example app listening on port 4000`);
 })
