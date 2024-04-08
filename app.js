f47e9f70d04dad5597feeb121025932a29265c89
BloodyNatuz <nathuznoepupin@gmail.com>
2024-04-09 00:39:39 +0200

    🧐 Post  method

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var Twig = require("twig"),
     express = require('express'),
     app = express();
 var db = require("./database.js")
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'))
 
 // Twig options
 app.set("twig options", {
@@ -30,7 +34,29 @@ app.get('/api/villes', (req, res, next) => {
         }
     });
 })
+app.post('/api/villes', (req, res, next) => {
+    var errors=[];
+    var data = {
+        name: req.body.sentName,
+        lon: req.body.sentLon,
+        lat: req.body.sentLat,
+        lastdate: req.body.lastUpdate,
+        temperature: req.body.temp,
+        skystate:req.body.weather
+    };
+    var sql = `INSERT INTO ville (name, lon, lat, lastdate, temperature, skystate) VALUES (?,?,?,?,?,?)`;
+    var params = [data.name, data.lon, data.lat, data.lastdate, data.temperature, data.skystate];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "data": data
+        })
+    })
+})
 
 app.listen(4000, () => {
-    console.log(`Example app listening on port 4000`);
+    console.log(`App listening on port 4000`);
 })
